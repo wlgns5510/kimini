416a0b1ca5fd2d9ceb0587563dda3dfa9eee7286
5335510 <5335510@naver.com>
2022-09-20 23:27:55 +0900

    2022 09-20

---
+++ src/main/java/kimino/controller.java
@@ -0,0 +1,15 @@
+package kimino;
+
+public class controller {
+
+	public static void main(String[] args) {
+		int a = 5;
+		double b = (double)a;
+		System.out.println(b);
+		
+		int c = (int)b;
+		System.out.println(c);
+
+	}
+
+}
